6f40c3cdbc6d7b6358f4195346b4deda80f2b860
Yuning <Yuning>
2017-06-17 17:14:36 +0800

    modify new to new1

--- application/controllers/pages/Sites.php
+++ application/controllers/pages/Sites.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sites extends CI_Controller {
-    public function new()
+    public function new1()
     {
         $datas = [
             'title' => '创建代理网站',

9b398fc5d2b1e803354fcdf1c49ab7aac479c4fa
Wu Hong <wuhong40@163.com>
2017-06-18 14:49:41 +0800

    增加产品管理界面

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -19,5 +19,60 @@ class Product_model extends Taoke_Model {
             $this->db->insert('Product', $row);
         }
     }
+
+    /**
+     * @Brief  获取表格Ajax请求所需的记录
+     *
+     * @Param $user
+     * @Param $query
+     * @Param $offset
+     * @Param $limit
+     * @Param $sort
+     * @Param $order
+     *
+     * @Returns
+     */
+    public function table_rows($query, $offset, $limit, $sort, $order)
+    {
+        $this->load->database();
+        $this->db->select("*");
+        $this->db->from("Product");
+        if($query) {
+            $this->db->group_start()
+                        ->or_like('D_title', $query)
+                        ->or_like('GoodsID', $query)
+                    ->group_end();
+        }
+        $this->db->limit($limit, $offset);
+        if($sort) {
+            $this->db->order_by($sort, $order);
+        }
+
+        return $this->db->get()->result_array();
+    }
+
+    /**
+     * @Brief 获取表格Ajax请求需要的记录行数
+     *
+     * @Param $user
+     * @Param $query
+     *
+     * @Returns
+     */
+    public function table_count($query)
+    {
+        $this->load->database();
+        $this->db->select("count(*) as cnt");
+        $this->db->from("Product");
+
+        if($query) {
+            $this->db->group_start()
+                        ->or_like('D_title', $query)
+                        ->or_like('GoodsID', $query)
+                    ->group_end();
+        }
+
+        return $this->db->get()->row()->cnt;
+    }
 }
 

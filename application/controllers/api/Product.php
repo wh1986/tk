d532156ab03dd9fd6d20035df51f0a4d5dbb778b
Wu Hong <wuhong40@163.com>
2017-06-26 22:12:55 +0800

    get domain

--- application/controllers/api/Product.php
+++ application/controllers/api/Product.php
@@ -124,6 +124,7 @@ class Product extends Api_Controller {
         $data = $this->product_to_json($product);
         $data['ModelTxt'] = "复制框内整段文字，打开手机淘宝即可「领取优惠券」并购买$tpwd->model";
         $data['domain'] = $domain;
+        $data["headers"] = $this->input->request_headers();
 
         response_exit(0, 'OK', $data);
     }
